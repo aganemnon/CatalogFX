2e6d44aec779a7ec2a013c62f7434906570da7a9
aganemnon <theaganrmnon@gmail.com>
2018-04-27 23:29:17 +0400

    rebase project

--- network/src/main/java/ru/netcracker/ibublig/network/TCPConnectionListener.java
+++
@@ -1,9 +0,0 @@
-package ru.netcracker.ibublig.network;
-
-public interface TCPConnectionListener {
-
-    void onConnectionReady(TCPConnection tcpConnection);
-    void onReceiveByte(TCPConnection tcpConnection, Object value);
-    void onDisconnect(TCPConnection tcpConnection);
-    void onException(TCPConnection tcpConnection, Exception e);
-}
